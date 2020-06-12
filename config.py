b6f22465c93c50fe73c1033086bb2ab2b2a359dc
Gonzalo Ferretti <gferretti@wetcom.net>
2020-06-12 10:19:04 -0300

    Further cleanup

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ LOGGING_LEVEL = logging.INFO
 LOGGING_HANDLER = logging.StreamHandler()
 LOGGING_FORMATTER = logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
 
-APPLICATION_HOST = os.environ.get("DEVOPS_JIRA_HOST",default="http://127.0.0.1:5000")
+APPLICATION_HOST = os.environ.get("HOST",default="http://127.0.0.1:5000")
 
 SWAGGER_TEMPLATE = {
   "swagger": "2.0",
@@ -15,14 +15,10 @@ SWAGGER_TEMPLATE = {
     "description": "API description",
     "contact": {
       "author": "Wetcom",
-      "url": "http://www.wetcom.com",
+      "url": "",
     },
     "version": "0.1.1"
   },
   "host": APPLICATION_HOST,
   "basePath": "/",  # base bash for blueprint registration
 }
-
-JIRA_HOST = "https://wetcom.atlassian.net"
-JIRA_CRED_USER = "devops@wetcom.com"
-JIRA_CRED_PWRD = os.environ.get("DEVOPS_JIRA_PWRD",default="")
